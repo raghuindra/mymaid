8867fc6a289adc6d7ea90f68b8a32cea4d6a70f0
Raghavendra Patil <raghuindra.p@gmail.com>
2017-03-05 03:10:31 +0530

    notify

--- application/views/block/footer.php
+++ application/views/block/footer.php
@@ -70,6 +70,49 @@
         });
     </script>
 <?php } ?>
+    
+    
+<script>
+    $(function(){
+<?php if($this->session->flashdata('error_message') != null ) {?>
+        var msg = "<?php echo $this->session->flashdata('error_message');?>";
+        
+        notifyMessage('error', msg);
+        
+<?php }else if($this->session->flashdata('success_message') != null ) { ?>  
+        var msg = "<?php echo $this->session->flashdata('success_message');?>";
+        notifyMessage('success', msg);
+
+<?php } ?>
+    
+    });
+    
+    function notifyMessage(type, msg){
+        noty({
+                text: msg,
+                type: type,
+                theme: 'defaultTheme',
+                dismissQueue: true,
+                layout: 'topRight',
+                timeout: 6000,
+                template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+                progressBar: true,
+                animation: {
+                open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+                close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+                easing: 'swing',
+                speed: 500 // opening & closing animation speed
+            },
+            closeWith: ['click']
+        });
+    }
+
+</script>
+
+<!-- Notyfy Notifications Plugin -->
+<script src="<?php echo plugin_url("notifications/notyfy/packaged/jquery.noty.packaged.js") ?>"></script>
+<script src="<?php echo plugin_url("notifications/notyfy/themes/default.js") ?>"></script>
+
 </body>
 
 </html>
